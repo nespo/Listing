dd88f56563b86b2414bb04c1b3b4a4494d7fbf3b
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-06-27 21:50:01 +0300

    Added log in admin

--- myproject/settings.py
+++ myproject/settings.py
@@ -169,17 +169,17 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'file': {
+        'console': {
             'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/path/to/your/logfile.log',
+            'class': 'logging.StreamHandler',
         },
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],
+            'handlers': ['console'],
             'level': 'DEBUG',
             'propagate': True,
         },
     },
 }
+
