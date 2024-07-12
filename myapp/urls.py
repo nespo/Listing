97c4f4f755c8f382cacd2676e3c213ddf129f379
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-07-12 12:56:22 +0300

    Change package to membership

--- myapp/urls.py
+++ myapp/urls.py
@@ -52,5 +52,5 @@ urlpatterns = [
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
     #admin
-    path('admin/formfieldsetting/', FormFieldSettingAdmin.changelist_view),
+    path('admin/myapp/formfieldsetting/', FormFieldSettingAdmin.changelist_view),
 ]
