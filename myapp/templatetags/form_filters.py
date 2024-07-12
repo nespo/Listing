dc3846c038b84df007d4c6fee9a47bf8b5a17245
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-07-12 15:11:12 +0300

    Change package to membership

--- myapp/templatetags/form_filters.py
+++ myapp/templatetags/form_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def get_field(form, field_name):
     return form[field_name]
+
+@register.filter
+def not_in_list(value, arg):
+    return value not in arg
